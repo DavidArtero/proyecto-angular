d408edfee151ec832b39ca55b5cfc2bac3c6d4a0
Davidama <davidarteromartinez@gmail.com>
2019-06-25 19:24:50 +0200

    Versión 1.0

---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var jQuery:any;
+declare var $:any;
+
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  public widthSlider: number;
+  public anchuraToSlider: any;
+  public captions: boolean;
+  public autor: any;
+
+  constructor() { 
+    this.captions = false;
+    this.autor = " ";
+  }
+
+  ngOnInit() {
+
+  }
+
+  cargarSlider(){
+    this.anchuraToSlider = this.widthSlider;
+  }
+
+  resetearSlider(){
+   this.anchuraToSlider = false;
+  }
+
+  getAutor(event){
+    this.autor = event;
+  }
+}
